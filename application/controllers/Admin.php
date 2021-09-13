b8476db83a6a8d187f42f45e5bbb263aca865deb
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2021-09-13 22:00:06 +0700

    Upload files

---
+++ application/controllers/Admin.php
@@ -0,0 +1,79 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		is_logged_in();
+	}
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+		$data['user'] = $this->db->get_where('user',['email' =>$this->session->userdata('email')])->row_array();
+		$data['title'] = 'Dashboard';
+		$this->load->view('templates/header',$data);
+		$this->load->view('templates/sidebar',$data);
+		$this->load->view('templates/topbar',$data);
+		$this->load->view('admin/index',$data);
+		$this->load->view('templates/footer',$data);
+	}
+
+	public function role()
+	{
+		// $this->load->view('welcome_message');
+		$data['user'] = $this->db->get_where('user',['email' =>$this->session->userdata('email')])->row_array();
+		$data['title'] = 'Role';
+
+		$data['role'] = $this->db->get('user_role')->result_array();
+		$this->load->view('templates/header',$data);
+		$this->load->view('templates/sidebar',$data);
+		$this->load->view('templates/topbar',$data);
+		$this->load->view('admin/role',$data);
+		$this->load->view('templates/footer',$data);
+	}
+
+
+	public function roleaccess($role_id)
+	{
+		// $this->load->view('welcome_message');
+		$data['title'] = 'Role Access';
+		$data['user'] = $this->db->get_where('user',['email' =>$this->session->userdata('email')])->row_array();
+		
+		$data['role'] = $this->db->get_where('user_role',['id'  => $role_id])->row_array();
+
+		$this->db->where('id !=',1);
+
+		$data['menu'] = $this->db->get('user_menu')->result_array();
+		$this->load->view('templates/header',$data);
+		$this->load->view('templates/sidebar',$data);
+		$this->load->view('templates/topbar',$data);
+		$this->load->view('admin/role-access',$data);
+		$this->load->view('templates/footer',$data);
+	}
+
+	public function changeAccess(){
+		$menu_id = $this->input->post('menuId');
+		$role_id = $this->input->post('roleId');
+
+		$data = [
+			'role_id' => $role_id,
+			'menu_id' => $menu_id
+		];
+
+		$result = $this->db->get_where('user_access_menu',$data);
+
+		if($result->num_rows() < 1){
+			$this->db->insert('user_access_menu',$data);
+		}else{
+			$this->db->delete('user_access_menu',$data);
+		}
+
+		$this->session->set_flashdata('message','
+			<div class="alert alert-success" role="alert">
+				Access Change
+			</div>
+			');
+	}
+}
